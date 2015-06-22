a4ff46807f8719928bb578026fe14cb156b6beca
pliso_c <pliso_c@git.epitech.eu>
2015-06-22 10:01:42 +0200

    lol

---
+++ skip.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+## skip.sh for skip in /home/pliso_c/Piscine/Synthese/automakefile
+## 
+## Made by christian pliso
+## Login   <pliso_c@epitech.net>
+## 
+## Started on  Mon Jun 22 09:10:16 2015 christian pliso
+## Last update Mon Jun 22 09:56:02 2015 christian pliso
+##
+
+awk '{print $3, $9}' | grep $1
